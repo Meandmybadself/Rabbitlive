7080f08146e65d4bf16d5744292b8762656d3f7f
Jeffery Bennett <meandmybadself@gmail.com>
2015-01-16 00:31:26 -0600

    Animating SVGs.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+var turq = '#34CCB0';
+
 function toggleRabbits() {
 	var mc = $('.main-content');
 	if (mc.hasClass('showingRabbits')) {
@@ -24,6 +26,43 @@ $(function() {
 		// TweenMax.to('#logo-text-item-1', .4, {width:'100px', delay:2.5});
 		// TweenMax.to('#logo-text-item-2', .4, {width:'100px', delay:2.5});
 
+		//TweenMax.from('.properties dt', 3, {opacity:0});
+
+
+
+		TweenMax.staggerTo('.properties dt', 1, {opacity:1}, 1);
+		TweenMax.staggerTo('.properties dd.details', .5, {css:{'opacity':1}, ease:Quad.easeOut}, 1);
+
+
+
+		TweenMax.set('.icon-path', {drawSVG:"0%, 0%"});
+		var stpTL = new TimelineMax();
+		stpTL.repeat(-1);
+		stpTL.staggerTo('.icon-path', 2, {drawSVG:"0% 50%", ease:Quad.easeIn, delay:0}, 1);
+		stpTL.to('.icon-path', 2, {drawSVG:"50% 50%", ease:Quad.easeOut, delay:0});
+
+		TweenMax.from($('.rabbit-logo-path'), 2, {drawSVG:"0% 0%", ease:Quad.eastOut});
+
+
+	});
+
+	$('.properties dl').on('mouseover', function() {
+		TweenMax.to($(this).find('.icon-path'), 1.5, {css:{'stroke':turq}});
+	});
+
+	$('.properties dl').on('mouseout', function() {
+		TweenMax.to($(this).find('.icon-path'), 1.5, {css:{'stroke':'#ffffff'}});
+	});
+
+	$('.meet-button').on('mouseover', function() {
+		TweenMax.staggerTo($(this).find('.rabbit-logo-path'), .75, {drawSVG:"0%", yoyo:true, repeat:-1, ease:Quad.easeInOut}, .2);
+	});
+
+	$('.meet-button').on('mouseout', function() {
+		TweenMax.to($(this).find('.rabbit-logo-path'), 1, {drawSVG:"100%", repeat:0, yoyo:false});
+	});
+	$('.meet-button').on('mouseleave', function() {
+		TweenMax.to($(this).find('.rabbit-logo-path'), 1, {drawSVG:"100%", repeat:0, yoyo:false});
 	});
 
 
